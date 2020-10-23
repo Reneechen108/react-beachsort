f197318d91671895c13960057dbe616737367ceb
Reneechen108 <reneechen108@gmail.com>
2020-10-23 16:00:07 -0700

    Update UserStore Code

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -4,7 +4,7 @@ class UserStore{
     constructor(){
         extendObservable(this,{
             loading: true,
-            isLoggIn: false,
+            isLoggedIn: false,
             username: ''
         })
     }
